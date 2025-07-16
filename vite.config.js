74320720ed8a8563ab9ccfeaafa09a02c505e9ae
Momen Zaki <mohammedmomen690@email.com>
2025-07-16 21:54:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default // https://vite.dev/config/
