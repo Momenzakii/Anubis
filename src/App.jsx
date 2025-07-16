f3350240aa43549586cb131caee938f107afa591
Momen Zaki <mohammedmomen690@email.com>
2025-07-16 21:56:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -10,7 +10,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        
       </Routes>
     </Router>
   );
