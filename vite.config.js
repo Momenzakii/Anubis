b1ca4c434669a5b97a2bd7eda03f709388256264
Momen Zaki <mohammedmomen690@email.com>
2025-07-16 21:53:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+export default // https://vite.dev/config/
+defineConfig({
+  base: "/Anubis/", // اسم الريبو بين سلاشات
 
-// https://vite.dev/config/
-export default defineConfig({
   plugins: [tailwindcss(), react()],
 });
